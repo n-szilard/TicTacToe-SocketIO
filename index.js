ac4d9fa455f48a8454ae9948eb77753f5d34557b
n-szilard <nagyapatiszilard@turr.hu>
2026-01-14 08:17:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const ejs = require('ejs');
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+server.listen(3000, ()=>{
+    console.log(`http://localhost:3000`);
+});
