72d02c01f5ef532caaae71df69e1c7fd35709ae5
n-szilard <nagyapatiszilard@turr.hu>
2026-01-20 09:27:26 +0100

    Csatlakozás egy szobához

--- index.js
+++ index.js
@@ -13,6 +13,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/game/:room', (req, res) => {
+    res.render('game', { roomCode: req.params.room });
+});
+
+app.get('/main', (req, res) => {
+    const {username, roomCode} = req.query;
+
+    const roomConfig = {
+        username,
+        roomCode
+    }
+
+    res.render('main', (roomConfig))
+})
+
+// tictactoe socket
+io.on('connection', (socket) => {
+
+    console.log(`Új felhasználó csatlakozott: ${socket.id}`);
+
+    socket.on('join-room', ({username, room}) => {
+        socket.join(room);
+        socket.to(room).emit('user-connected', username);
+    });
+
+    socket.on('leave-room', ({username, room}) => {
+        socket.leave(room);
+        socket.to(room).emit('user-disconnected', username);
+    });
+
+    socket.on('make-move', ({room, position}) => {
+        socket.to(room).emit('move-made', {position, player: socket.id});
+    });
+
+    socket.on('restart-game', (room) => {
+        socket.to(room).emit('game-restarted');
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Felhasználó lecsatlakozott: ${socket.id}`);
+    });
+});
 
 server.listen(3000, ()=>{
     console.log(`http://localhost:3000`);
--- public/client.js
+++ public/client.js
@@ -1,1 +1,10 @@
 const socket = io();
+
+let joinBtn = document.getElementById('joinBtn');
+
+joinBtn.addEventListener('click', () => {
+    console.log('asd')
+    const username = nameInput.value;
+    const room = codeInput.value;
+    socket.emit('join-room', { username, room });
+});
